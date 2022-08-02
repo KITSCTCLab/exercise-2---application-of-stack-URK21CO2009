af28aca9f1fbb14d396a12c945fe6e273fbd8e2b
URK21CO2009 <110015313+URK21CO2009@users.noreply.github.com>
2022-08-02 10:50:48 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -25,6 +25,10 @@ class Evaluate:
       True if it is empty, else returns False.
     """
       # Write your code here
+    if len(self.stack) == 0:
+        return True
+    else:
+        return False
 
 
   def pop(self):
@@ -34,6 +38,9 @@ class Evaluate:
       The data which is popped out if the stack is not empty.
     """
     # Write your code here
+    if len(self.stack) > 0:
+        x = self.stack.pop()
+        return x
 
 
   def push(self, operand):
@@ -43,6 +50,7 @@ class Evaluate:
       operand: The operand to be pushed.
     """
     # Write your code here
+    self.stack.append(operand)
 
 
   def validate_postfix_expression(self, expression):
@@ -54,6 +62,17 @@ class Evaluate:
       True if the expression is valid, else returns False.
     """
     # Write your code here
+    value = True
+    
+    valid = ['+','-','*','/']
+    
+    for char in expression:
+        if char.isdigit or char in valid:
+            continue
+        else:
+            value = False
+    
+    return value
 
 
   def evaluate_postfix_expression(self, expression):
@@ -64,14 +83,25 @@ class Evaluate:
     Returns:
       The result of evaluated postfix expression.
     """
+    
     # Write your code here
-
-
-# Do not change the following code
-postfix_expression = input()  # Read postfix expression
-tokens = postfix_expression.split()
-evaluate = Evaluate(len(tokens))
-if evaluate.validate_postfix_expression(tokens):
-    print(evaluate.evaluate_postfix_expression(tokens))
-else:
-    print('Invalid postfix expression')
+    
+    for char in expression:
+        if char.isdigit():
+            self.push(char)
+        else:
+            b = int(self.pop())
+            a = int(self.pop())
+            
+            if char == "+":
+                result = a + b
+            elif char == "-":
+                result = a - b
+            elif char == '*':
+                result = a * b
+            elif char == '/':
+                result = a / b
+            
+            self.push(result)
+    
+    return int(self.stack[0])
